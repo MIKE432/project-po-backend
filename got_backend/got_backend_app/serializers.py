bb04a6b1d00409ce9764343c8c564dfb8bfa6123
Mateusz Groblicki <lokigameplayer@gmail.com>
2021-01-17 17:04:29 +0100

    added part of serializers, added gitignore cleanup

--- got_backend/got_backend_app/serializers.py
+++ got_backend/got_backend_app/serializers.py
@@ -1,9 +1,7 @@
-import re
 from typing import Dict
 from django.db import models
-from django.db.models import fields
 from rest_framework import serializers
-from got_backend_app.models import GrupaGorska, Region
+from got_backend_app.models import GrupaGorska, Legitymacja, Odcinek, Region, Ksiazeczka
 
 
 class RegionSerializer(serializers.ModelSerializer):
@@ -31,7 +29,6 @@ class GrupaGorskaSerializer(serializers.ModelSerializer):
         model = GrupaGorska
         fields = ['kodgrupy', 'nazwa', 'idreg']
 
-
     def create(self, validated_data: Dict) -> GrupaGorska:
         """
         Create and return a new `GrupaGorska` instance, given the validated data.
@@ -42,7 +39,85 @@ class GrupaGorskaSerializer(serializers.ModelSerializer):
         """
         Update and return an existing `GrupaGorska` instance, given the validated data.
         """
-        instance.nazwa=validated_data.get('nazwa', instance.nazwa)
-        instance.idreg=validated_data.get('idreg', instance.idreg)
+        instance.nazwa = validated_data.get('nazwa', instance.nazwa)
+        instance.idreg = validated_data.get('idreg', instance.idreg)
         instance.save()
         return instance
+
+
+class KsiazeczkaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ksiazeczka
+        fields = ['idksiazki', 'idturysty', 'punktacja']
+
+    def create(self, validated_data: Dict) -> Ksiazeczka:
+        """
+        Create and return a new `Ksiazeczka` instance, given the validated data.
+        """
+        return Ksiazeczka.objects.create(**validated_data)
+
+    def update(self, instance: Ksiazeczka, validated_data: Dict) -> Ksiazeczka:
+        """
+        Update and return an existing `Ksiazeczka` instance, given the validated data.
+        """
+        instance.punktacja = validated_data.get(
+            'punktacja', instance.punktacja)
+        instance.save()
+        return instance
+
+
+class LegitymacjaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Legitymacja
+        fields = ['nrlegi', 'idprzod', 'terminwazn', 'dataprzyznania']
+
+    def create(self, validated_data: Dict) -> Legitymacja:
+        """
+        Create and return a new `Legitymacja` instance, given the validated data.
+        """
+        return Legitymacja.objects.create(**validated_data)
+
+    def update(self, instance: Legitymacja, validated_data: Dict) -> Legitymacja:
+        """
+        Update and return an existing `Legitymacja` instance, given the validated data.
+        """
+        instance.terminwazn = validated_data.get(
+            'terminwazn', instance.terminwazn)
+        instance.dataprzyznania = validated_data.get(
+            'dataprzyznania', instance.terminwazn)
+        instance.save()
+        return instance
+
+
+class OdcinekSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Legitymacja
+        fields = ['idodc', 'idpoczatek', 'idkoniec', 'nazwa', 'przewyzszenie',
+                  'dlugosc', 'czyaktywny', 'dataponotw', 'punktacja', 'rokakt', 'discriminator']
+
+    def create(self, validated_data: Dict) -> Odcinek:
+        """
+        Create and return a new `Odcinek` instance, given the validated data.
+        """
+        return Odcinek.objects.create(**validated_data)
+
+    def update(self, instance: Odcinek, validated_data: Dict) -> Odcinek:
+        """
+        Update and return an existing `Odcinek` instance, given the validated data.
+        """
+        instance.nazwa = validated_data.get('nazwa', instance.nazwa)
+        instance.przewyzszenie = validated_data.get(
+            'przewyzszenie', instance.przewyzszenie)
+        instance.dlugosc = validated_data.get('dlugosc', instance.dlugosc)
+        instance.czyaktywny = validated_data.get(
+            'czyaktywny', instance.czyaktywny)
+        instance.dataponotw = validated_data.get(
+            'dataponotw', instance.dataponotw)
+        instance.punktacja = validated_data.get(
+            'punktacja', instance.punktacja)
+        instance.discriminator = validated_data.get(
+            'discriminator', instance.discriminator)
+        instance.rokakt = validated_data.get('rokakt', instance.rokakt)
+        instance.save()
+        return instance
+        
